a161295d60c6d34f0405bfe911183f73aeda4bb6
Adapa Uma Siva Sankar <umasivashankaradapa@gmail.com>
2026-03-09 19:58:40 +0530

    Fix: Hardcode JWT expiry to 30d

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || '30d',
+    expiresIn: '30d',
   });
 
   // Since it's an API, we can either return the token or set it in a cookie.
